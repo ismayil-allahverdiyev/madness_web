20af6c7094c4bfa09b2e4e60d3fa326e756ec410
Ismayil Allahverdiyev <allahverdiyev.ismayl@mail.ru>
2024-03-03 19:32:09 +0300

    home-background fixed

--- src/modules/home/layout/Header.tsx
+++ src/modules/home/layout/Header.tsx
@@ -1,11 +1,12 @@
 import { NavBar } from "../../shared/features/nav_bar";
 import { ContactLargeView } from "../features/contact_large_feature";
+import HomeBackground from "../../../assets/images/home_background.png";
+
 
 export function Header() {
     return <>
-        <div className="min-h-screen max-h-screen bg-home-background bg-cover bg-center bg-no-repeat relative">
-            <NavBar />
-            <ContactLargeView />
-        </div>
+        <img src={HomeBackground} className={`min-h-screen max-h-screen w-screen relative`} />
+        <NavBar />
+        <ContactLargeView />
     </>;
 }

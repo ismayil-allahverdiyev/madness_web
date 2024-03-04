ad009c54c37c1c536c785385be06dd4d4e39f07f
Ismayil Allahverdiyev <allahverdiyev.ismayl@mail.ru>
2024-03-04 10:31:15 +0300

    gender size updated

--- src/modules/calculation/components/gender_selector.tsx
+++ src/modules/calculation/components/gender_selector.tsx
@@ -10,7 +10,7 @@ interface GenderSelectorProps {
 export default function GenderSelector(props: GenderSelectorProps) {
     var { isMale, handleIsMaleState } = useContext(CalculationContext);
 
-    return <div onClick={() => handleIsMaleState(props.isMale)} className={`lg:w-[400px] lg:h-[500px] md:w-[360px] md:h-[450px] sm:w-[280px] sm:h-[350px] w-[200px] h-[250px]  border shadow-lg ${(props.isMale) ? "hover:shadow-blue-500 hover:text-blue-500" : "hover:shadow-pink-500 hover:text-pink-500"} ${(props.isMale && isMale) ? "text-blue-500 shadow-blue-500" : (!props.isMale && !isMale) ? "shadow-pink-500 text-pink-500" : "text-gray-500"} mx-4 lg:p-16 p-12 rounded-3xl p-auto`}>
+    return <div onClick={() => handleIsMaleState(props.isMale)} className={`lg:w-[400px] lg:h-[500px] md:w-[360px] md:h-[450px] sm:w-[280px] sm:h-[350px] w-[180px] h-[220px]  border shadow-lg ${(props.isMale) ? "hover:shadow-blue-500 hover:text-blue-500" : "hover:shadow-pink-500 hover:text-pink-500"} ${(props.isMale && isMale) ? "text-blue-500 shadow-blue-500" : (!props.isMale && !isMale) ? "shadow-pink-500 text-pink-500" : "text-gray-500"} mx-4 lg:p-16 p-12 rounded-3xl p-auto`}>
         {props.isMale ? <IoMan className="w-[100%] h-[100%]" /> : <IoWoman className="w-[100%] h-[100%]" />}
     </div>
 }
